98212e5c5e2fd61151d99119096f6ad4b78e8b7c
gnavia <ginavia12@gmail.com>
2025-07-02 22:57:08 -0400

    creacion del componente login html

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup,Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb:FormBuilder){
+    this.loginForm=this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
+
+
+  onSubmit():void{
+    if(this.loginForm.valid){
+      console.log('Login ', this.loginForm.value);
+    }
+  }
 
 }
